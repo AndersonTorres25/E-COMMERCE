8ff9aedaa55d5a85cfc92778be6ddeca4125ab63
Anderson Torres <barcelona.torres@estudantes.ifg.edu.br>
2026-09-22 15:10:23 -0300

    Atualizaçao logica js

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,21 +1,108 @@
 
+//ANCO DE USUÁRIOS
 
-document.addEventListener("DOMContentLoaded", function () {
-    
-    const formLogin = document.getElementById("form-login");
+const usuarios = [
+    {
+        nome: "Administrador",
+        email: "admin@shop.com",
+        senha: "123456"
+    },
+    {
+        nome: "Anderson",
+        email: "anderson@email.com",
+        senha: "123"
+    }
+];
 
-    formLogin.addEventListener("submit", function (event) {
-        event.preventDefault();
+// LOGIN
 
-        const emailInput = document.getElementById("campo-login").value;
-        const senhaInput = document.getElementById("campo-senha").value;
+const formulario = document.querySelector("#form-login");
 
-        if (emailInput !== "" && senhaInput !== "") {
-            alert(`Login realizado com sucesso!\nBem-vindo(a), ${emailInput}`);
-            
+if (formulario) {
+
+    formulario.addEventListener("submit", function (e) {
+
+        e.preventDefault();
+
+        const email = document.querySelector("#email").value;
+        const senha = document.querySelector("#senha").value;
+
+        const usuario = usuarios.find(function (u) {
+            return u.email === email && u.senha === senha;
+        });
+
+        if (usuario) {
+
+            localStorage.setItem(
+                "usuarioLogado",
+                JSON.stringify(usuario)
+            );
+
+            alert("Login realizado com sucesso!");
+
             window.location.href = "../index.html";
+
         } else {
-            alert("Por favor, preencha todos os campos.");
+
+            alert("E-mail ou senha inválidos.");
+
         }
+
     });
-});
+
+}
+
+// VERIFICAR USUÁRIO LOGADO
+
+function atualizarNavbar() {
+
+    const dados = localStorage.getItem("usuarioLogado");
+
+    if (!dados) return;
+
+    const usuario = JSON.parse(dados);
+
+    document.querySelector("#nomeUsuario").innerHTML =
+        usuario.nome;
+
+    document.querySelector("#menuLogin").style.display = "none";
+    document.querySelector("#menuCadastro").style.display = "none";
+
+    document.querySelector("#menuPerfil").style.display = "block";
+    document.querySelector("#menuSair").style.display = "block";
+
+}
+
+atualizarNavbar();
+
+const btnSair = document.querySelector("#btnSair");
+
+if (btnSair) {
+
+    btnSair.addEventListener("click", function () {
+
+        localStorage.removeItem("usuarioLogado");
+
+        window.location.href = "../index.html";
+
+    });
+
+}
+const mostrar = document.querySelector("#mostrarSenha");
+
+if (mostrar) {
+
+    mostrar.addEventListener("change", function () {
+
+        const senha = document.querySelector("#senha");
+
+        if (this.checked) {
+            senha.type = "text";
+        } else {
+            senha.type = "password";
+        }
+
+    });
+
+}
+
